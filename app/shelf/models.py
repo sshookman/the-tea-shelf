b30eabd1ce4d54279b31a17b16b2024dc6b0692b
Sean Shookman <sms112788@gmail.com>
2022-10-15 21:29:00 -0500

    basic setup for index of shelf, tea details, and timer pages

--- app/shelf/models.py
+++ app/shelf/models.py
@@ -5,8 +5,8 @@ class Tea(models.Model):
     variety = models.CharField(max_length=100) # Green, Oolong, Black, White, Fruit, Herbal, etc.
     source = models.CharField(max_length=100) # The Source from which the Tea was Procured
     steep_time = models.IntegerField(default=120) # Time in Seconds
-    steep_temp = models.IntegerField(default=112) # Temp in Fahrenheit
+    steep_temp = models.IntegerField(default=210) # Temp in Fahrenheit
     tsp_per_cup = models.FloatField(default=1) # Teaspoons Per Cup
 
-    def __str__(str):
-        return f"{self.name} {self.variety} Tea from {source}"
+    def __str__(self):
+        return f"{self.name} {self.variety} Tea from {self.source}"
--- app/shelf/urls.py
+++ app/shelf/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:tea_id>/', views.detail, name='detail'),
+    path('<int:tea_id>/timer/', views.timer, name='timer'),
 ]
