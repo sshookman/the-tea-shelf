1e27248e25101595deeb715df800e377bf3600eb
Sean Shookman <sms112788@gmail.com>
2022-10-15 21:29:52 -0500

    basic setup for index of shelf, tea details, and timer pages (#3)

--- app/shelf/views.py
+++ app/shelf/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from .models import Tea
 
 def index(request):
-    username = "Sean"
-    return HttpResponse(f"Welcome to {username}'s Tea Shelf")
+    teas = Tea.objects.order_by('-name')
+    return render(request, 'shelf/index.html', {'teas':teas})
+
+def detail(request, tea_id):
+    tea = get_object_or_404(Tea, pk=tea_id)
+    return render(request, 'shelf/details.html', {'tea':tea})
+
+def timer(request, tea_id):
+    tea = get_object_or_404(Tea, pk=tea_id)
+    return render(request, 'shelf/timer.html', {'tea':tea})
