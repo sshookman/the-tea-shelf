6932f9eb508b7f87d66910c2f16f69e094b96635
Sean Shookman <sms112788@gmail.com>
2022-10-19 21:12:54 -0500

    Basic Form (#4)
    * need to add timer
    
    * basic form

--- app/shelf/urls.py
+++ app/shelf/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add', views.add, name='add'),
+    path('form', views.form, name='form'),
     path('<int:tea_id>/', views.detail, name='detail'),
-    path('<int:tea_id>/timer/', views.timer, name='timer'),
+    path('<int:tea_id>/timer/', views.timer, name='timer')
 ]
