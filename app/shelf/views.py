4cdd3d15b0cffca729be3239c78c480040377af9
Sean Shookman <sms112788@gmail.com>
2022-10-19 21:11:48 -0500

    basic form

--- app/shelf/views.py
+++ app/shelf/views.py
@@ -1,9 +1,18 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 from .models import Tea
 
 def index(request):
     teas = Tea.objects.order_by('-name')
     return render(request, 'shelf/index.html', {'teas':teas})
+
+def form(request):
+    return render(request, 'shelf/form.html')
+
+def add(request):
+    print(request.POST['name'])
+    return HttpResponseRedirect(reverse('form'))
 
 def detail(request, tea_id):
     tea = get_object_or_404(Tea, pk=tea_id)
